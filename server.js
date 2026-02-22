b3103d78aa24c430e065ed817ac4c5413a75f3f9
Haris Sabanovic <hasa2303@student.miun.se>
2026-02-22 18:16:57 +0100

    små ändring

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const BlogPostRoutes = require("./Routes/BlogPostRoutes");
 
 const server = Hapi.server({
     port: process.env.PORT || 4000,
-    host:"localhost",
+    host:"0.0.0.0",
     routes: {
         cors:{
             origin: ["*"],
